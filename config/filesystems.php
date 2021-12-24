91b15ea47949ff9c7befe8f897b89fa91214f6f7
Zé Cipriano <ze@zecipriano.com>
2021-12-24 11:02:13 +0000

    Upload profile photos to cloud disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,15 +42,13 @@ return [
             'visibility' => 'public',
         ],
 
-        's3' => [
+        'cloud' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'key' => env('DO_SPACES_KEY'),
+            'secret' => env('DO_SPACES_SECRET'),
+            'region' => env('DO_SPACES_REGION'),
+            'bucket' => env('DO_SPACES_BUCKET'),
+            'endpoint' => env('DO_SPACES_ENDPOINT'),
         ],
 
     ],
