64179d18e4589c36f7ab348bc4859206e7362e34
Zé Cipriano <ze@zecipriano.com>
2021-12-27 10:47:55 +0000

    Add dark mode to auth views

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -37,8 +37,8 @@
 
                             <div class="ml-2">
                                 {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',
+                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="hover:underline text-sm text-amber-700 hover:text-amber-900">'.__('Terms of Service').'</a>',
+                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="hover:underline text-sm text-amber-700 hover:text-amber-900">'.__('Privacy Policy').'</a>',
                                 ]) !!}
                             </div>
                         </div>
@@ -47,7 +47,7 @@
             @endif
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                <a class="text-sm text-amber-700 hover:text-amber-900 hover:underline dark:text-amber-600 dark:hover:text-amber-100" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
                 </a>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,17 @@
         @if (Route::has('login'))
             <div class="fixed top-0 right-0 px-6 py-4 block">
                 @auth
-                    <a href="{{ url('/dashboard') }}" class="text-sm text-amber-700 hover:text-amber-900 hover:underline dark:text-amber-600 dark:hover:text-amber-100">Dashboard</a>
+                    <a href="{{ url('/dashboard') }}" class="text-base text-amber-700 hover:text-amber-900 hover:underline dark:text-amber-600 dark:hover:text-amber-100">Dashboard</a>
                 @else
-                    <a href="{{ route('login') }}" class="text-sm text-amber-700 hover:text-amber-900 hover:underline dark:text-amber-600 dark:hover:text-amber-100">Log in</a>
+                    <a href="{{ route('login') }}" class="text-base text-amber-700 hover:text-amber-900 hover:underline dark:text-amber-600 dark:hover:text-amber-100">Log in</a>
 
                     @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="ml-4 text-sm text-amber-700 hover:text-amber-900 hover:underline dark:text-amber-600 dark:hover:text-amber-100">Register</a>
+                        <a href="{{ route('register') }}" class="ml-4 text-base text-amber-700 hover:text-amber-900 hover:underline dark:text-amber-600 dark:hover:text-amber-100">Register</a>
                     @endif
                 @endauth
             </div>
         @endif
 
-        <div class="text-stone-700 underline dark:text-stone-300 decoration-amber-600 text-7xl md:text-9xl font-bold text-center">Cheers</div>
+        <div class="text-stone-700 dark:text-stone-300 text-7xl md:text-9xl font-bold text-center">Cheers</div>
     </div>
 </x-guest-layout>
